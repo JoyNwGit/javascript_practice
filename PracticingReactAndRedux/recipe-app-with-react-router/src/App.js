1a149c3b48632b77b77bf8eab9a6f036eabd50b5
Joy <supergaljoy1994@gmail.com>
2021-08-22 23:03:17 -0700

    added some comments

--- PracticingReactAndRedux/recipe-app-with-react-router/src/App.js
+++ PracticingReactAndRedux/recipe-app-with-react-router/src/App.js
@@ -22,19 +22,18 @@ class App extends Component {
   getRecipe = async (event) => {
     // get the words in the input box
     const recipeName = event.target.elements.recipeName.value; 
-    event.preventDefault(); // avoid a full page refresh as default behaviour
-    const api_call = await fetch(`https://api.spoonacular.com/recipes/${tempSearchType}?apiKey=${API_KEY}&number=${number}`);
     // console.log(recipeName);
+    event.preventDefault(); // avoid a full page refresh as default behaviour
+    const api_call = await fetch
+    (`https://api.spoonacular.com/recipes/${tempSearchType}?apiKey=${API_KEY}&query=${recipeName}&number=${number}`);
     const response = api_call.json(); // actually retrieve the data
     response.then((data) => {
       this.setState({ recipes: data.results })
-      // console.log(data.results[0].title); // "Stuff worked"
+      console.log(this.state.recipes); // "Stuff worked"
     }, function(err){
       console.log(err) // it broke
-    }
-    )
-    
-  }
+    })// end of response promise
+  } // end of getRecipes fn
   render() {
     return (
       <div className="App">
@@ -42,6 +41,22 @@ class App extends Component {
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe}/>
+        {
+          //dont forget the key for react!
+          // btw the ?. is like a checker for if a function/object/reference thing exists that
+          // can be used to prevent crashes. add them from right to left: ?.map then ?.recipes, etc. 
+          // i say this because if something in between isnt there like recipes, it'll still try to run map
+          // and potentially crash
+          this.state?.recipes?.map((recipe) => {
+            // to avoid the key warning, put the key in the most parent element!
+            return (
+              <div key={recipe.id}>
+              <img src={recipe.image} alt={recipe.title}/>
+                <p>{recipe.title}</p>
+              </div>
+              )
+          })
+        }
       </div>
     );
   }
